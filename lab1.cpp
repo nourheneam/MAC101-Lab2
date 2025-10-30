7857ca24bdf2a4cf206841fda87a5257fbbbf146
nourheneam <nourheneameri25@gmail.com>
2025-10-30 16:07:34 -0400

    Lab1

---
+++ lab1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string name;
+    cout << "What is your name?";
+
+    string year;
+    cout << "What is your year in college?";
+
+    string movie;
+    cout << "What is your favorite movie?";
+
+    cout << "My name is Nourhene Ameri." << endl;
+    cout << "I'm a second year student." << endl;
+    cout << "My favorite movie is Titanic." << endl;
+
+    return 0;
+}
